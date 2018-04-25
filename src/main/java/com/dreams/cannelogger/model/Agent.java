5f7346ef46fdb5b7068b219c189c08d3fead5699
Farad77 <farad77@gmail.com>
2018-04-25 14:29:08 +0400

    new

---
+++ src/main/java/com/dreams/cannelogger/model/Agent.java
@@ -0,0 +1,31 @@
+package com.dreams.cannelogger.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Agent {
+
+	@Id
+	@GeneratedValue	
+	private Long id;
+	
+	private String nom;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+}
